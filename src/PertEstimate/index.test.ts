69fbfc0756e0eec54379a73a5471c72c6762b31b
Andrey Khar'kovets <a.harkovets@gmail.com>
2021-08-15 00:53:44 +0300

    Tests fixed

--- src/PertEstimate/index.test.ts
+++ src/PertEstimate/index.test.ts
@@ -15,5 +15,30 @@ it('correctly counts estimate (1/1/1)', () => {
   const [O, ML, P] = [1, 1, 1];
   const threePointEstimate = new ThreePointEstimate(O, ML, P);
   const PERT = new PertEstimate(threePointEstimate);
-  expect(typeof PERT.estimate).toBe('number');
+  expect(PERT.estimate).toBe((O + (ML * 4) + P) / 6);
+  expect(PERT.standartDeviation).toBe((P - O) / 6);
+});
+
+it('correctly counts estimate (1/2/4)', () => {
+  const [O, ML, P] = [1, 2, 4];
+  const threePointEstimate = new ThreePointEstimate(O, ML, P);
+  const PERT = new PertEstimate(threePointEstimate);
+  expect(PERT.estimate).toBe((O + (ML * 4) + P) / 6);
+  expect(PERT.standartDeviation).toBe((P - O) / 6);
+});
+
+it('correctly counts estimate (4.5/8.2/11.5)', () => {
+  const [O, ML, P] = [4.5, 8.2, 11.5];
+  const threePointEstimate = new ThreePointEstimate(O, ML, P);
+  const PERT = new PertEstimate(threePointEstimate);
+  expect(PERT.estimate).toBe((O + (ML * 4) + P) / 6);
+  expect(PERT.standartDeviation).toBe((P - O) / 6);
+});
+
+it('correctly counts estimate (30/45/60)', () => {
+  const [O, ML, P] = [30, 45, 60];
+  const threePointEstimate = new ThreePointEstimate(O, ML, P);
+  const PERT = new PertEstimate(threePointEstimate);
+  expect(PERT.estimate).toBe((O + (ML * 4) + P) / 6);
+  expect(PERT.standartDeviation).toBe((P - O) / 6);
 });
